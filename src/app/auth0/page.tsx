2d35bffd111d5e277eec9f3a3829415bb872d20f
zaru <zarutofu@gmail.com>
2024-03-22 13:31:42 +0900

    show Auth0 session

--- src/app/auth0/page.tsx
+++ src/app/auth0/page.tsx
@@ -1,3 +1,14 @@
-export default function Page() {
-  return <div>protected page</div>;
+import { getSession } from "@auth0/nextjs-auth0";
+
+export default async function Page() {
+  const session = await getSession();
+  if (!session) {
+    return <div>unauthenticated</div>;
+  }
+
+  return (
+    <div>
+      protected page<div>login {session.user.email}</div>
+    </div>
+  );
 }
